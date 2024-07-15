07306cad2b93238f12d5d318724b065c209ebecd
marco <marcoramirezmail@gmail.com>
2024-07-15 15:41:32 -0400

    scaffold

---
+++ src/components/navbar.js
@@ -0,0 +1,20 @@
+import styles from "../scss/comp.module.scss";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <main>
+      <h1>About Us</h1>
+      <p>
+        Climb Time Indy is the best climbing gym in Indianapolis. We offer a
+        variety of climbing walls, fitness classes, and gear for sale.
+      </p>
+    </main>
+  );
+}
