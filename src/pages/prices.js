557473fe1ffb49e9203f2491e4a0ab02998f5c2d
Brandon Kesner <77083072+rebelmaniac@users.noreply.github.com>
2024-07-18 11:55:56 -0400

    price formating

--- src/pages/prices.js
+++ src/pages/prices.js
@@ -4,22 +4,27 @@ export default function Prices() {
   return (
     <section>
       <h1>Prices</h1>
-      <h2>Passes</h2>
-      {price_data.prices.Passes.map((pass, index) => (
-        <div key={index}>
-          <h3>
-            {pass.name} - {pass.price}
-          </h3>
-        </div>
-      ))}
-      <h2>Membership Prices</h2>
-      {price_data.prices.Memberships.map((membership, index) => (
-        <div key={index}>
-          <h3>
-            {membership.timePeriod} - {membership.price}
-          </h3>
-        </div>
-      ))}
+      <div>
+        <h2>Passes</h2>
+        {price_data.prices.Passes.map((pass, index) => (
+          <div key={index}>
+            <h3>
+              {pass.name} - {pass.price}
+            </h3>
+          </div>
+        ))}
+        <h2>
+          Membership Prices
+          <span style={{ fontSize: "10px" }}> *prepaid or reoccurring</span>
+        </h2>
+        {price_data.prices.Memberships.map((membership, index) => (
+          <div key={index}>
+            <h3>
+              {membership.timePeriod} - {membership.price}
+            </h3>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
