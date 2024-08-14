6be22bc9872bc8084d3dcad4a396a3658e63b817
Brandon Kesner <77083072+rebelmaniac@users.noreply.github.com>
2024-08-14 13:47:38 -0400

    improve about page social media buttons plus potential page layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,56 +1,65 @@
 import MapDisplay from "@/components/map";
+import styles from "@/scss/pages/about.module.scss";
+import { FacebookLogo, InstagramLogo } from "@phosphor-icons/react";
+
+import Image from "next/image";
+import photo1 from "../assets/photo0.png";
 
 export default function About() {
   return (
     <section>
-      <h1>About Us</h1>
-      <p>
-        Since 1997, Climb Time Indy is dedicated to to providing the best that
-        rock climbing has to offer. We are focused on maintaining a safe
-        environment where climbers of all ability levels and ages can get
-        better, learn more and above all else, have fun climbing. Climb Time
-        offers a wide range of difficulty levels from very easy to moderate to
-        extremely difficult, brought to you by some of the best route setters
-        this side of the Mississippi. Routes are changed weekly in order to
-        ensure there is always some thing new to challenge yourself with.
-        Whether you are looking for powerful bouldering, sustained routes, or
-        just a day out with the family, Climb Time Indy has what you are looking
-        for. Come on by; we’d love for you to come climbing with us.
-      </p>
+      <div className={styles.container}>
+        <div className={styles.aboutContainerContent}>
+          <div className={styles.aboutPhotoContainer}>
+            <Image
+              src={photo1}
+              alt="photo1"
+              width="fill"
+              height="fill"
+              layout="responsive"
+            />
+          </div>
+          <div className={styles.divLineVertical}></div>
+          <div className={styles.aboutTextContainer}>
+            <h1 className={styles.headerText}>About Us</h1>
+            <p className={styles.paragraphText}>
+              Since 1997, Climb Time Indy is dedicated to to providing the best
+              that rock climbing has to offer. We are focused on maintaining a
+              safe environment where climbers of all ability levels and ages can
+              get better, learn more and above all else, have fun climbing.
+              Climb Time offers a wide range of difficulty levels from very easy
+              to moderate to extremely difficult, brought to you by some of the
+              best route setters this side of the Mississippi. Routes are
+              changed weekly in order to ensure there is always some thing new
+              to challenge yourself with. Whether you are looking for powerful
+              bouldering, sustained routes, or just a day out with the family,
+              Climb Time Indy has what you are looking for. Come on by; we’d
+              love for you to come climbing with us.
+            </p>
+          </div>
+        </div>
+      </div>
       <MapDisplay />
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
+      <div className={styles.container}>
+        <hr className={styles.hrLine} />
+      </div>
+      <div className={styles.container}>
         <div style={{ display: "flex" }}>
           <button
-            style={{
-              backgroundColor: "inherit",
-              border: "1px solid",
-              margin: "2.5px",
-              padding: "2px",
-            }}
+            className={styles.socialMediaButton}
             onClick={() =>
               window.open("https://www.facebook.com/climbtimeindy")
             }
           >
-            Facebook
+            <FacebookLogo className={styles.socialMediaIcon} size={36} />
           </button>
           <button
-            style={{
-              backgroundColor: "inherit",
-              border: "1px solid",
-              margin: "2.5px",
-              padding: "2px",
-            }}
+            className={styles.socialMediaButton}
             onClick={() =>
               window.open("https://www.instagram.com/climbtimeindy/")
             }
           >
-            Instagram
+            <InstagramLogo className={styles.socialMediaIcon} size={36} />
           </button>
         </div>
       </div>
