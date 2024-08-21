343a42e1dfe0a46adfdd4bd5db7b779534954ab7
marco <marcoramirezmail@gmail.com>
2024-08-21 16:29:42 -0400

    started mdx integration. got all settings adjusted

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,26 @@
 export default {
   webpack: (cfg) => {
+    // Existing loader for .md files
     cfg.module.rules.push({
       test: /\.md$/,
       loader: "frontmatter-markdown-loader",
       options: { mode: ["react-component"] },
     });
+
+    // New loader for .mdx files
+    cfg.module.rules.push({
+      test: /\.mdx$/,
+      use: [
+        {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+        "@mdx-js/loader",
+      ],
+    });
+
     return cfg;
   },
 };
--- src/pages/parties.js
+++ src/pages/parties.js
@@ -1,22 +1,48 @@
-import { attributes, react as HomeContent } from "../../content/ctiHome.md";
+import { useEffect, useState } from "react";
+import matter from "gray-matter";
+import { promises as fs } from "fs";
+import path from "path";
+import { MDXRemote } from "next-mdx-remote";
+import { serialize } from "next-mdx-remote/serialize";
 
-export default function Parties() {
-  let { title, cats } = attributes;
+export default function Parties({ attributes, mdxSource }) {
+  const [stateAttributes, setStateAttributes] = useState(attributes);
+
+  useEffect(() => {
+    setStateAttributes(attributes);
+  }, [attributes]);
+
+  const { title, examples } = stateAttributes;
 
   return (
     <section>
       <article>
         <h1>{title}</h1>
-        <HomeContent />
         <ul>
-          {cats.map((cat, k) => (
+          {examples.map((example, k) => (
             <li key={k}>
-              <h2>{cat.name}</h2>
-              <p>{cat.description}</p>
+              <h4>{example.name}</h4>
+              <h5>{example.price}</h5>
+              <p>{example.description}</p>
             </li>
           ))}
         </ul>
+        <MDXRemote {...mdxSource} />
       </article>
     </section>
   );
+}
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "content", "newToClimbing.mdx");
+  const fileContent = await fs.readFile(filePath, "utf-8");
+  const { data: attributes, content } = matter(fileContent);
+  const mdxSource = await serialize(content);
+
+  return {
+    props: {
+      attributes,
+      mdxSource,
+    },
+  };
 }
