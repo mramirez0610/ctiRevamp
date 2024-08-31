aa2461503962405486273735c2c8ff14f0e27a98
marco <marcoramirezmail@gmail.com>
2024-08-31 17:52:59 -0400

    real test

--- src/pages/about.js
+++ src/pages/about.js
@@ -18,52 +18,52 @@ import {
   PictureRightTextLeft,
 } from "@/components/pictureText";
 
-// const components = {
-//   ImageComponent: (props) => (
-//     <div className={styles.container}>
-//       <div className={styles.aboutContainerContent}>
-//         <div className={styles.aboutPhotoContainer}>
-//           <ImageComponent
-//             {...props}
-//             photo={photo1}
-//             alt="photo1"
-//             className={styles.photoContainer}
-//           />
-//         </div>
-//         <div className={styles.divLineVertical}></div>
-//         <div className={styles.aboutTextContainer}>
-//           <h1 className={styles.headerText}>About Us</h1>
-//           <p className={styles.paragraphText}>
-//             Since 1997, Climb Time Indy is dedicated to to providing the best
-//             that rock climbing has to offer. We are focused on maintaining a
-//             safe environment where climbers of all ability levels and ages can
-//             get better, learn more and above all else, have fun climbing. Climb
-//             Time offers a wide range of difficulty levels from very easy to
-//             moderate to extremely difficult, brought to you by some of the best
-//             route setters this side of the Mississippi. Routes are changed
-//             weekly in order to ensure there is always some thing new to
-//             challenge yourself with. Whether you are looking for powerful
-//             bouldering, sustained routes, or just a day out with the family,
-//             Climb Time Indy has what you are looking for. Come on by; we’d love
-//             for you to come climbing with us.
-//           </p>
-//         </div>
-//       </div>
-//     </div>
-//   ),
-//   PictureLeftTextRight: (props) => <PictureLeftTextRight {...props} />,
-//   PictureRightTextLeft: (props) => <PictureRightTextLeft {...props} />,
-//   MapDisplay,
-//   SocialMediaButtons,
-//   div: (props) => <div className={styles.container}>{props.children}</div>,
-// };
-
-// export default function AboutPage({ source }) {
-//   return <MDXRemote {...source} components={components} />;
-// }
+const components = {
+  ImageComponent: (props) => (
+    <div className={styles.container}>
+      <div className={styles.aboutContainerContent}>
+        <div className={styles.aboutPhotoContainer}>
+          <ImageComponent
+            {...props}
+            photo={photo1}
+            alt="photo1"
+            className={styles.photoContainer}
+          />
+        </div>
+        <div className={styles.divLineVertical}></div>
+        <div className={styles.aboutTextContainer}>
+          <h1 className={styles.headerText}>About Us</h1>
+          <p className={styles.paragraphText}>
+            Since 1997, Climb Time Indy is dedicated to to providing the best
+            that rock climbing has to offer. We are focused on maintaining a
+            safe environment where climbers of all ability levels and ages can
+            get better, learn more and above all else, have fun climbing. Climb
+            Time offers a wide range of difficulty levels from very easy to
+            moderate to extremely difficult, brought to you by some of the best
+            route setters this side of the Mississippi. Routes are changed
+            weekly in order to ensure there is always some thing new to
+            challenge yourself with. Whether you are looking for powerful
+            bouldering, sustained routes, or just a day out with the family,
+            Climb Time Indy has what you are looking for. Come on by; we’d love
+            for you to come climbing with us.
+          </p>
+        </div>
+      </div>
+    </div>
+  ),
+  PictureLeftTextRight: (props) => <PictureLeftTextRight {...props} />,
+  PictureRightTextLeft: (props) => <PictureRightTextLeft {...props} />,
+  MapDisplay,
+  SocialMediaButtons,
+  div: (props) => <div className={styles.container}>{props.children}</div>,
+};
 
 export default function AboutPage({ source }) {
-  return <MDXRemote {...source} />;
+  return (
+    <>
+      <MDXRemote {...source} components={components} />
+    </>
+  );
 }
 
 // export default function About({ attributes, mdxSource }) {
