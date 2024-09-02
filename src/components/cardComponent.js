b8d68ccaf686131b9d4b2755165378eafce7516c
marco <marcoramirezmail@gmail.com>
2024-09-02 11:49:17 -0400

    partial 3d animation

--- src/components/cardComponent.js
+++ src/components/cardComponent.js
@@ -1,9 +1,42 @@
 import Image from "next/image";
 import styles from "@/scss/components/cardComponent.module.scss";
+import { useRef } from "react";
 
 export default function CardComponent({ title, description, image }) {
+  // const boundingRef = (useRef < DOMRect) | (null > null);
+  const boundingRef = useRef(null);
+
+  const handleMouseEnter = (e) => {
+    boundingRef.current = e.target.getBoundingClientRect();
+    e.target.style.transform = `rotateX(0deg) rotateY(0deg)`;
+  };
+
+  const handleMouseMove = (e) => {
+    if (!boundingRef.current) return;
+
+    const x = e.clientX - boundingRef.current.left;
+    const y = e.clientY - boundingRef.current.top;
+
+    const xPercent = x / boundingRef.current.width;
+    const yPercent = y / boundingRef.current.height;
+
+    const xRotation = (0.5 - xPercent) * 20;
+    const yRotation = (0.5 - yPercent) * 20;
+
+    e.target.style.transform = `perspective(800px) rotateX(${-yRotation}deg) rotateY(${xRotation}deg)`;
+  };
+
+  const handleMouseLeave = () => {
+    boundingRef.current = null;
+  };
+
   return (
-    <div className={styles.card}>
+    <div
+      className={styles.card}
+      onMouseEnter={handleMouseEnter}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+    >
       <Image
         src={image}
         alt={title}
