49396562b40a625bf56ab6fb8fac33d667d616eb
marco <marcoramirezmail@gmail.com>
2024-09-16 12:48:51 -0400

    optimization

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,9 @@
 import Hero from "@/components/hero";
 import Head from "next/head";
 import Script from "next/script";
-
-import { promises as fs } from "fs";
-import { useEffect, useState } from "react";
-import matter from "gray-matter";
-import path from "path";
 import NavigationCards from "@/components/navigationCards";
 
-export default function Home({ attributes }) {
-  const [stateAttributes, setStateAttributes] = useState(attributes);
-
-  useEffect(() => {
-    setStateAttributes(attributes);
-  }, [attributes]);
-
+export default function Home() {
   return (
     <>
       <Head>
@@ -27,16 +16,4 @@ export default function Home({ attributes }) {
       <NavigationCards />
     </>
   );
-}
-
-export async function getStaticProps() {
-  const filePath = path.join(process.cwd(), "content", "ctiHome.mdx");
-  const fileContent = await fs.readFile(filePath, "utf-8");
-  const { data: attributes } = matter(fileContent);
-
-  return {
-    props: {
-      attributes,
-    },
-  };
 }
