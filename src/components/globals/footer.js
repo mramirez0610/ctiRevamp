28545705faa06cae129225d748c299bda67d10ab
marco <marcoramirezmail@gmail.com>
2025-09-26 09:35:43 -0700

    fixed sass depreciation -- footer redesign start

--- src/components/globals/footer.js
+++ src/components/globals/footer.js
@@ -5,22 +5,25 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       {/* <MapDisplay /> */}
-      <p>&copy; 2024 Climb Time Indy</p>
-      <p>8750 Corporation Dr, Indianapolis, IN 46256</p>
+      <div className={styles.backdrop}></div>
+      <div className={styles.footerInfo}>
+        <p>&copy; 2024 Climb Time Indy</p>
+        <p>8750 Corporation Dr, Indianapolis, IN 46256</p>
 
-      <div className={styles.hours}>
-        <h4>Gym hours:</h4>
-        M-F 11am – 10pm
+        <div className={styles.hours}>
+          <h4>Gym hours:</h4>
+          M-F 11am – 10pm
+          <br />
+          SAT 9am – 10pm
+          <br />
+          SUN 11am – 10pm
+        </div>
         <br />
-        SAT 9am – 10pm
-        <br />
-        SUN 11am – 10pm
-      </div>
-      <br />
-      <div className={styles.contact}>
-        <h4>Get in Touch:</h4>
-        <p>Phone: 317-596-3335</p>
-        <p>Email: info@ClimbTimeIndy.com</p>
+        <div className={styles.contact}>
+          <h4>Get in Touch:</h4>
+          <p>Phone: 317-596-3335</p>
+          <p>Email: info@ClimbTimeIndy.com</p>
+        </div>
       </div>
     </footer>
   );
